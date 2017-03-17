6dc146b9c703240e6f0dd2bf2a0f17ce7ed4619d
JasminBecerra <jbecerra2@wisc.edu>
2017-03-16 22:31:14 -0500

    updating data

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 // main js file
+
+//begin script when window loads
+window.onload = setMap();
+
+//set up choropleth map
+function setMap(){
+    //use d3.queue to parallelize asynchronous data loading
+    d3.queue()
+        .defer(d3.csv, "data/ChicagoSocioEconData.csv") //load attributes from csv
+        .defer(d3.json, "data/ChicagoComm6P.topojson") //load background spatial data
+        .await(callback);
+};'
