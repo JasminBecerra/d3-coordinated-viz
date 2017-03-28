18ef9d5d5eba82c3855d651657ababad69b4437e
JasminBecerra <jbecerra2@wisc.edu>
2017-03-28 00:58:40 -0500

    replaced original spatial data

--- js/main.js
+++ js/main.js
@@ -5,8 +5,9 @@ window.onload = setMap();
 
 //set up choropleth map
 function setMap(){
-	//actualmap dimensions
-	var w = 1000, h = 500;
+	//actualmap dimensions (w = width, h = height)
+	var w = 1000, 
+        h = 500;
 
 	//container for map
 	var actualmap = d3.select("body")
@@ -15,24 +16,41 @@ function setMap(){
 		.attr("width", w)
 		.attr("height", h);
 
-		
+	//create Albers equal area conic projection centered on Chicago (used the UW Cart demo)
+    var projection = d3.geoAlbers() // try geo.albers
+		.center([10.92, 48.16])
+		.rotate([99.20, 7.29, 0])
+		.parallels([41.79, 41.88])
+		.scale(5000.00)
+		.translate([w / 2, h / 2]);
+
+	//create path generator for actualmap
+	var path = d3.geoPath()
+    	.projection(projection);
 
 
     //use d3.queue to parallelize asynchronous data loading
     d3.queue()
         .defer(d3.csv, "data/ChicagoSocioEconData.csv") //load attributes from csv
-        .defer(d3.json, "data/ChicagoComm6P.topojson") //load spatial data for choropleth map
+        .defer(d3.json, "data/chicagoNeighborhoods.topojson") //load spatial data for choropleth map
         .await(callback); //send data to callback function
 
     function callback(error, csvData, chicago){
     	//translate chicago comm areas to topojson
-    	var chicagoComm6p = topojson.feature(chicago, chicago.objects.ChicagoComm6P).features;
+    	var chicagoNeighborhoods = topojson.feature(chicago, chicago.objects.chicagoNeighorhoods).features;
         
-    	//check
-        // console.log(error);
+        var comms = actualmap.append("path")
+        	.data(chicagoNeighborhoods)
+        	.enter()
+        	.append("path")
+        	.attr("class", function(d){
+                return "regions " + d.properties.adm1_code;
+            })
+            .attr("d", path);
+    	// check
+        console.log(error);
         console.log(csvData);
         console.log(chicago);
     };
+
 };
-
-
