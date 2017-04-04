e7ac9aee6ee06d6b7895fde7249be712572437c5
JasminBecerra <jbecerra2@wisc.edu>
2017-04-03 22:47:45 -0500

    joining cdv data to topojson

--- js/main.js
+++ js/main.js
@@ -32,14 +32,14 @@ function setMap(){
 
     //use d3.queue to parallelize asynchronous data loading
     d3.queue()
-        .defer(d3.csv, "data/ChicagoSocioEconData.csv") //load attributes from csv
-        .defer(d3.json, "data/illinCounties2.topojson") //load bacground data (illinois counties)
+        .defer(d3.csv, "data/ChicagoSocioEconData3.csv") //load attributes from csv
+        .defer(d3.json, "data/illinCounties3.topojson") //load bacground data (illinois counties)
         .defer(d3.json, "data/ChicagoCommunities.topojson") //load spatial data for choropleth map
+        // .defer(d3.json, "data/lakemich.topojson") //load lake background lake michigan data
         .await(callback); //send data to callback function
 
 
-//still debugging this, something is off, and I've checked to make sure I have the correct
-//objects and properties names according to the topojson files
+//function to populate the dom with topojson data
     function callback(error, csvData, illinois, chicago){
         //*ignore* graticule doesn't make much sense for the scale at which I'm mapping
         // //create graticule generator
@@ -47,14 +47,43 @@ function setMap(){
         //     .step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
 
     	//translate chicago comm areas to topojson
-    	var illinCounties = topojson.feature(illinois, illinois.objects.illinCounties2),
+    	var illinCounties = topojson.feature(illinois, illinois.objects.illinCounties3),
             chicagoCommunities = topojson.feature(chicago, chicago.objects.ChicagoCommunities).features;
         
+            
+        //variables for data join (I had to revise the original csv file because there were some spaces before every entry, so it wouldn't join initially)
+        var attrArray = ["HardshipIndex", "PerCapitaIncome", "PercentAged16Unemployed", "PercentAged25WithoutHighSchoolDiploma", "PercentAgedUnder18orover64", "PercentHouseholdsBelowPoverty", "PercentofHousingCrowded"];
+
+        //loop through csv to assign each set of csv attribute values to geojson region
+        for (var i=0; i<csvData.length; i++){
+            var csvRegion = csvData[i]; //the current region
+            var csvKey = csvRegion.community; //the CSV primary key = community
+
+            //loop through geojson regions to find correct region
+            for (var a=0; a<chicagoCommunities.length; a++){
+
+                var geojsonProps = chicagoCommunities[a].properties; //the current region geojson properties
+                var geojsonKey = geojsonProps.community; //the geojson primary key
+
+                //where primary keys match, transfer csv data to geojson properties object
+                if (geojsonKey == csvKey){
+
+                    //assigning attributes and values
+                    attrArray.forEach(function(attr){
+                        var val = parseFloat(csvRegion[attr]); //get csv attribute value
+                        geojsonProps[attr] = val; //assign attribute and value to geojson properties
+                    });
+                };
+            };
+        };
+
+
         //ading the illinois counties to actualmap
         var counties = actualmap.append("path")
             .datum(illinCounties)
             .attr("class", "counties")
             .attr("d", path);
+
 
         //adding chicago community areas/neighborhoods to actualmap
         var communities = actualmap.selectAll(".communities")
@@ -70,7 +99,7 @@ function setMap(){
     	// // check
         console.log(error);
         console.log(csvData);
-        console.log(illinois);
+        // console.log(illinois);
         console.log(chicago);
     };
 
